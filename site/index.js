f6dd279915178298469fd3562cbdc671317ef647
garzoku <garzoku@WGC10316TQCB3.zld.dc.ford.com>
2022-03-30 16:17:26 -0500

    add css rules

--- site/index.js
+++ site/index.js
@@ -1,21 +1,42 @@
-const body = document.querySelector("body")
-// create a pokemon listing
-const listingDiv = document.createElement("div")
-listingDiv.className = "pokemon - listing"
-// create fig
-const listingFig = document.createElement("figure")
-// create img
-const listingImage = document.createElement("img")
-listingImage.src = "pokemon-image-url-goes-here.jpg"
-listingImage.alt = "Pokemon Name Goes Here"
-// create figcaption
-const listingFigCaption = document.createElement("figcaption")
-const listingA = document.createElement("a")
-listingA.href = "pokemon.html?pokemon=pokemon-id-goes-here"
-listingA.textContent = "Pokemon Name Goes Here"
-// append items
-body.append(listingDiv)
-listingDiv.append(listingFig)
-listingFig.append(listingImage)
-listingFig.append(listingFigCaption)
-listingFigCaption.append(listingA)
+const app = document.querySelector("#app")
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=50")
+    .then(response => response.json())
+    .then(response => {
+        const pokemonList = response.results
+        pokemonList.map(pokemon => pokemon)
+    })
+
+
+const $ul = document.querySelector(".pokemon")
+
+buildListing()
+buildListing()
+buildListing()
+
+function buildListing() {
+
+    const $li = document.createElement("li")
+    // create a pokemon listing
+    const listingDiv = document.createElement("div")
+    listingDiv.className = "pokemon-listing"
+    // create fig
+    const listingFig = document.createElement("figure")
+    // create img
+    const listingImage = document.createElement("img")
+    listingImage.src = "pokemon-image-url-goes-here.jpg"
+    listingImage.alt = "Pokemon Name Goes Here"
+    // create figcaption
+    const listingFigCaption = document.createElement("figcaption")
+    const listingA = document.createElement("a")
+    listingA.href = "pokemon.html?pokemon=pokemon-id-goes-here"
+    listingA.textContent = "Pokemon Name Goes Here"
+    // append items
+    $ul.append($li)
+    $li.append(listingDiv)
+    listingDiv.append(listingFig)
+    listingFig.append(listingImage)
+    listingFig.append(listingFigCaption)
+    listingFigCaption.append(listingA)
+
+}
